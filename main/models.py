89bf791b02726534f13f17817433dbaf6e0bea14
Tushar <tushar.mohan2001@gmail.com>
2019-12-21 23:43:12 +0530

    login complete with reset password

--- main/models.py
+++ main/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 # Create your models here.
+'''
+Custom model manager
+'''
+class UserProfileManager(models.Manager):
+    def get_queryset(self):
+        return super(UserProfileManager,self).get_queryset().order_by('phone')
 """
 
 Every time you make a new model
@@ -20,6 +26,14 @@ class UserProfile(models.Model):
     link=models.CharField(max_length=256,default='')
     phone=models.IntegerField(default=0)
     image=models.ImageField(upload_to='profile_image',blank=True)
+    '''
+    Now UsserProfileManager allows me to access queryset in a way i defined
+    The variable name with which i define UserProfileManager can now be used to access the objects in shell 
+    '''
+    camera = UserProfileManager()
+    '''
+    camera can now be used in place of objects to access queryset
+    '''
     def __str__(self):
         return str(self.user.username)
 def create_profile(sender, **kwargs):
